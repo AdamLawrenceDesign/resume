9b689272cc8231a1d1091635288452a68bbe358c
adam <adam@tiltandco.com>
2017-08-21 14:48:28 +1000

    style variables updated

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,24 @@
-
 export class App {
 
-    constructor(container, sampleData) {
+  constructor(container, sampleData) {
 
-        this.container = container;
-        this.sampleData = sampleData;
+    this.container = container;
+    this.sampleData = sampleData;
 
-        // Audit Value
-        this.value = {};
+    // Audit Value
+    this.value = {};
 
-        // Setup allow routes
-        page('/', this.setup.bind(this));
-        page('*', this.setup.bind(this));
-        page({ hashbang: true });
+    // Setup allow routes
+    page('/', this.setup.bind(this));
+    page('*', this.setup.bind(this));
+    page({
+      hashbang: true
+    });
 
-    }
+  }
 
-    setup() {
-        console.log('hey lets do something!')
-    }
+  setup() {
+    console.log('hey lets do something!')
+  }
 
 }
